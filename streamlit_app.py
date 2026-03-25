fe2dbab2b3abc5a5c1cb96506305e0d49de9e337
Jubrf <brefie.julien@gmail.com>
2026-03-25 16:21:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,9 @@ from utils.ndvi_processing import (
 
 st.set_page_config(page_title="NDVI – Sentinel-2", layout="wide")
 
+st.write("USER =", st.secrets.get("julien.brefie@sdea.fr"))
+st.write("PASS LENGTH =", len(st.secrets.get("Jkp1!62Xq'81","")))
+
 st.title("🌱 Analyse NDVI des parcelles agricoles – Sentinel‑2")
 
 uploaded = st.file_uploader(
